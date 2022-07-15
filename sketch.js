d395d2f1546dc6e2db8d42b0432840d7c899771b
helloshaurya1997 <109024378+helloshaurya1997@users.noreply.github.com>
2022-07-15 18:21:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var i1;
+var i1,response,response1,min,hr;
 function preload(){
 i1=loadImage("i1.jpg");
 }
@@ -7,26 +7,27 @@ function setup(){
 }
 function draw(){
 getTime();
+if(hr >= 00 && hr <=11){
+    console.log("morrrning");
+}
+else if(hr >= 12 && hr <=16){
+    console.log("afternoon");
+}
+if(hr >= 17 && hr <=23){
+    console.log("evening");
+    background(i1);
+    text("Good Evening",windowWidth/2-100,windowHeight/2-100);
+    textSize(20);
+    fill("yellow");
+}
 }
 async function getTime(){
-    var response=await fetch("https://worldtimeapi.org/api/timezone/asia/kolkata")
-    var response1=await response.json();
-    var dateTime=response1.datetime;
-    var min=dateTime.slice(11,16);
-    var hr=dateTime.slice(11,13);
+    response=await fetch("https://worldtimeapi.org/api/timezone/asia/kolkata")
+    response1=await response.json();
+    dateTime=response1.datetime;
+    min=dateTime.slice(11,16);
+    hr=dateTime.slice(11,13);
     console.log(hr);
 
-    if(hr >= 00 && hr <=11){
-        console.log("morrrning");
-    }
-    else if(hr >= 12 && hr <=16){
-        console.log("afternoon");
-    }
-    if(hr >= 17 && hr <=23){
-        console.log("evening");
-        background(i1);
-        text("Good Evening",windowWidth/2-100,windowHeight/2-100);
-        textSize(20);
-        fill("yellow");
-    }
+   
 }
